32f6317afe16f6d51d9148d377ce99fd5a1ed3ea
Roger Sosa <rsosao@miumg.edu.gt>
2026-02-25 17:06:36 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-string l = Console.ReadLine(); 
+string l = Console.ReadLine();
 int n = int.Parse(l);
 string[] t = Console.ReadLine().Split(' ');
 int[] v = new int[n];
 for (int i = 0; i < n; i++)
-{
     v[i] = int.Parse(t[i]);
-}
 int m = v[0];
 for(var i = 1; i < n; i++)
-{
-    if (v[i] > m)
-    {
-        m = v[i];
-    }
-}
+    if (v[i] > m) m = v[i];
 Console.WriteLine(m);
